a2b89d5343097e7a261afd2fc5469b59d8d21c25
vytautasb <vytautas.buitvydas@gmail.com>
2017-11-20 11:31:42 +0200

    Change in project structure

--- ip/clkctrl_c5/clkctrl_c5/clkctrl_c5_inst.v
+++
@@ -1,6 +0,0 @@
-	clkctrl_c5 u0 (
-		.inclk  (<connected-to-inclk>),  //  altclkctrl_input.inclk
-		.ena    (<connected-to-ena>),    //                  .ena
-		.outclk (<connected-to-outclk>)  // altclkctrl_output.outclk
-	);
-
